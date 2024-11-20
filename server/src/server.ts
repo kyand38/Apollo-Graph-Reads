a496488cc669d110de72ab8613f3f7adb12503ce
kyand38 <kyand2024@gmail.com>
2024-11-20 17:59:24 -0600

    changed app.use in server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -27,7 +27,7 @@ context: authenticateToken }));
 
 // if we're in production, serve client/dist as static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/dist')));
+  app.use(express.static(path.join(process.cwd(),'client','dist')));
 
   app.get('*', (_req, res) => {
     res.sendFile(path.join(__dirname, '../client/dist/index.html'));
